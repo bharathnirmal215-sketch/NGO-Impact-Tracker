ecdcb01b809cf87fb8092c84071cb7850dfced0a
bharathnirmal215-sketch <bharathnirmal215@gmail.com>
2025-12-14 13:29:48 +0530

    fixed

--- backend/ngo_tracker/settings.py
+++ backend/ngo_tracker/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'whitenoise.runserver_nostatic',  # For static files in production
     'reports',
 ]
 
